4672741c6298cf6e6fa693429d49e5bcf2449dd0
guimileib <guilhermemileib@gmail.com>
2024-12-16 10:33:08 -0300

    conexão com cliente e css começado

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
 
-
-@app.route('/')
+# Rota que renderiza o template
+@app.route('/chat/connect', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Chat em Tempo Real")
 
-
+# Evento para mensagens enviadas
 @socketio.on('message')
 def handle_message(msg):
+    print(f"Mensagem recebida: {msg}")
     emit('message', msg, broadcast=True)
 
 
